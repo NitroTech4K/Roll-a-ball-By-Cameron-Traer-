e5e4bbd689ed60856f43a8e4a6c5de22ea275c50
NitroTech 4K <crtraer@gmail.com>
2022-09-23 14:57:53 -0300

    Added gate

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -41,7 +41,7 @@ public class PlayerController : MonoBehaviour
     void SetCountText()
     {
         countText.text = "Count: " + count.ToString();
-        if(count >= 12)
+        if (count >= 84) 
         {
             //winTextObject.SetActive(true);
             
